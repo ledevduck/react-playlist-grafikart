ab8a6db445c818b3f3f6f9b579e285d92add0c2b
devduck <devduck@tutanota.com>
2022-08-20 12:03:19 +0200

    read-only input because value is set in component and there is no method to update it for now

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Joe",
+    };
+  }
+
   render() {
     return (
       <div>
         <label htmlFor="name">My name</label>
-        <input type="text" id="name" name="name" />
+        <input type="text" id="name" name="name" value={this.state.name} />
       </div>
     );
   }
