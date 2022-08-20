0fde9b07c2dcb9971c2618d1cede7c9f7b5935f5
devduck <devduck@tutanota.com>
2022-08-20 10:38:26 +0200

    *** First Component

--- app.js
+++ app.js
@@ -1,29 +1,5 @@
-let n = 0;
-
-function numberFormat(n) {
-  return n.toString().padStart(2, "0");
+function Welcome(props) {
+  return <h1>Hello World</h1>;
 }
 
-function render() {
-  const items = ["Tache 1", "Tache 2", "Tache 3"];
-
-  const lis = items.map((item, k) => <li key={k}>{item}</li>);
-
-  const title = (
-    <React.Fragment>
-      <h1 className={"title" + n}>
-        Hello World <span>{n}</span>
-      </h1>
-      <ul>{lis}</ul>
-    </React.Fragment>
-  );
-
-  ReactDOM.render(title, document.querySelector("#app"));
-}
-
-render();
-
-window.setInterval(() => {
-  n++;
-  render();
-}, 1000);
+ReactDOM.render(<Welcome />, document.querySelector("#app"));
