5233dae7cfabd7a9ed9fcf33792dec247987e698
devduck <devduck@tutanota.com>
2022-08-20 12:32:07 +0200

    form example by first retrieving name by using setState method with [name] to only update name key, works with name and firstname but problem with checkbox

--- app.js
+++ app.js
@@ -2,27 +2,54 @@ class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      checked: true,
+      name: "",
+      firstName: "",
+      newsletter: false,
     };
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(e) {
+    const name = e.target.name;
     this.setState({
-      checked: e.target.checked,
+      [name]: e.target.value,
     });
   }
 
   render() {
     return (
       <div>
-        <label htmlFor="name">My name</label>
-        <input
-          type="checkbox"
-          checked={this.state.checked}
-          onChange={this.handleChange}
-        ></input>
-        {this.state.checked ? <div>CHECKED</div> : null}
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            value={this.state.name}
+            onChange={this.handleChange}
+            id="name"
+            name="name"
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="firstName">First Name</label>
+          <input
+            type="text"
+            value={this.state.firstName}
+            onChange={this.handleChange}
+            id="firstName"
+            name="firstName"
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="newsletter">Newsletter</label>
+          <input
+            type="checkbox"
+            checked={this.state.newsletter}
+            onChange={this.handleChange}
+            id="newsletter"
+            name="newsletter"
+          ></input>
+        </div>
+        {JSON.stringify(this.state)}
       </div>
     );
   }
