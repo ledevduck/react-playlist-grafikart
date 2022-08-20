7bf0b3404212e3c266b32c46125b79389e84adb2
devduck <devduck@tutanota.com>
2022-08-20 10:03:27 +0200

    add static span

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-const title = React.createElement("h1", {}, "Hello World");
+const title = React.createElement(
+  "h1",
+  {},
+  "Hello World ",
+  React.createElement("span", {}, 0)
+);
 
 ReactDOM.render(title, document.querySelector("#app"));
