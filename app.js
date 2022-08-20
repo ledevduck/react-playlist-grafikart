67b68b77b9af18c5ac21863291c912c435f669e8
devduck <devduck@tutanota.com>
2022-08-20 16:06:24 +0200

    add Column2 Component with left and right props to display inputs in column on large screens

--- app.js
+++ app.js
@@ -32,6 +32,15 @@ function SecondaryButton({ children }) {
   return <Button type="secondary">{children}</Button>;
 }
 
+function Column2({ left, right }) {
+  return (
+    <div className="row">
+      <div className="col-md-6">{left}</div>
+      <div className="col-md-6">{right}</div>
+    </div>
+  );
+}
+
 function BoilingVerdict({ celsius }) {
   if (celsius >= 100) {
     return <div className="alert alert-success">BOIL</div>;
@@ -101,15 +110,21 @@ class Calculator extends React.Component {
       scale === "f" ? temperature : tryConvert(temperature, toFahrenheit);
     return (
       <div>
-        <TemperatureInput
-          scale="c"
-          temperature={celsius}
-          onTemperatureChange={this.handleCelsiusChange}
-        />
-        <TemperatureInput
-          scale="f"
-          temperature={fahrenheit}
-          onTemperatureChange={this.handleFahrenheitChange}
+        <Column2
+          left={
+            <TemperatureInput
+              scale="c"
+              temperature={celsius}
+              onTemperatureChange={this.handleCelsiusChange}
+            />
+          }
+          right={
+            <TemperatureInput
+              scale="f"
+              temperature={fahrenheit}
+              onTemperatureChange={this.handleFahrenheitChange}
+            />
+          }
         />
         <BoilingVerdict celsius={celsius} />
         <PrimaryButton>Submit</PrimaryButton>
