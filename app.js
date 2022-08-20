6aff4896d5d3c53e14b0118fb252e199ef03f726
devduck <devduck@tutanota.com>
2022-08-20 10:44:52 +0200

    use class Component

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-function Welcome({ name, children }) {
+function WelcomeFunc({ name, children }) {
   return (
     <div>
       <h1>Hello {name}</h1>
       <p>{children}</p>
     </div>
   );
+}
+
+class Welcome extends React.Component {
+  render() {
+    console.log(this.props);
+    return <h1>Hello World</h1>;
+  }
 }
 
 ReactDOM.render(
